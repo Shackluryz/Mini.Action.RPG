b8669ea1b9e382aa6311cb4f793cabf61ea998a0
Shackluryz <alem.shackuryz@gmail.com>
2021-03-14 12:41:53 -0300

    Slime
    Programando o Hit, Die, HP e Corroutine

---
+++ Projeto Zelda/Assets/Scripts/SlimeIA.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeIA : MonoBehaviour {
+
+    private Animator anim;
+    public int HP;
+
+    private bool isDie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Died() {
+
+        isDie = true;
+        yield return new WaitForSeconds(2.5f);
+        Destroy(this.gameObject);
+    
+    }
+
+    #region Meus Métodos
+    void GetHit(int amount) {
+
+        if (isDie == true) return;
+
+        HP -= amount;
+
+        if (HP > 0) {
+
+            anim.SetTrigger("GetHit");
+
+        } else {
+
+            anim.SetTrigger("Die");
+            StartCoroutine("Died");
+            
+        }
+        
+    }
+
+    #endregion
+}
