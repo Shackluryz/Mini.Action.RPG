d9883a613c0bfd653d1d3f7f38d6798925ae2c3a
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2021-08-26 15:43:04 -0300

    [ UPDATE ] Slime IA
    Programando a Máquina de Estado - Parte 1

--- Projeto Zelda/Assets/Scripts/SlimeIA.cs
+++ Projeto Zelda/Assets/Scripts/SlimeIA.cs
@@ -8,12 +8,16 @@ public class SlimeIA : MonoBehaviour {
     public int HP;
     private bool isDie;
 
-    public enemyState state;    
+    public enemyState state;
 
+    public const float idleWaitTime = 3f;
+    public const float patrolWaitTime = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        ChangeState(state);
     }
 
     // Update is called once per frame
@@ -71,13 +75,45 @@ public class SlimeIA : MonoBehaviour {
 
     void ChangeState(enemyState newState)
     {
+        StopAllCoroutines();
+        state = newState;
+        print(newState);
         switch (state)
         {
             case enemyState.IDLE:
+                StartCoroutine("IDLE");
                 break;
             case enemyState.ALERT:
                 break;
+            case enemyState.PATROL:
+                StartCoroutine("PATROL");
+                break;
         }
+    }
+
+    IEnumerator IDLE()
+    {
+        yield return new WaitForSeconds(idleWaitTime);
+        if(Rand() < 50)
+        {
+            ChangeState(enemyState.IDLE);
+        }
+        else
+        {
+            ChangeState(enemyState.PATROL);
+        }
+    }
+
+    IEnumerator PATROL()
+    {
+        yield return new WaitForSeconds(patrolWaitTime);
+        ChangeState(enemyState.IDLE);
+    }
+
+    int Rand()
+    {
+        int rand = Random.Range(0, 100);
+        return rand;
     }
     #endregion
 }
